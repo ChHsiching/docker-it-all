6f734cd463da1bac6f33740229fc5a506336d883
ChHsiching <hsichingchang@gmail.com>
2024-12-17 10:42:20 +0800

    ✨ Feat: add open-webui & ollama
    https://aur.archlinux.org/packages/open-webui, Web UI and OpenAI API for various LLM runners, including Ollama

---
+++ open-webui/build/_app/immutable/chunks/clone.C5AsjNpw.js
@@ -0,0 +1,2 @@
+import{b as r}from"./graph.CNjZqN9J.js";var e=4;function a(o){return r(o,e)}export{a as c};
+//# sourceMappingURL=clone.C5AsjNpw.js.map
---
+++ open-webui/build/_app/immutable/entry/start.CEvl72sQ.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/entry.BsJDnQad.js";export{t as start};
+//# sourceMappingURL=start.CEvl72sQ.js.map
